6bbfc89be56508c088e5c69258decf6bf94ef9e4
Ion <ion.moraru.c@thalesdigital.io>
2019-03-03 19:24:02 +0100

    feat(add-skills-form): add and remove skills form

---
+++ src/components/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
+
+<style>
+    body {
+    background-color: #eeeeee;
+    font-family: "Montserrat", sans-serif;
+    display: grid;
+    grid-template-rows: auto;
+    justify-items: center;
+    padding-top: 50px;
+    }
+
+    nav {
+    padding: 20px 20px 20px 0;
+    }
+
+    nav a {
+    padding: 10px;
+    text-decoration: none;
+    background: #fff;
+    border-radius: 3px;
+    color: rgb(0, 110, 255);
+    font-weight: bold;
+    margin-right: 15px;
+    }
+</style>
